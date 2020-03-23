44294f13cc317573c7e95c0992dc62085df22df7
betonit007 <timnagorski@hotmail.com>
2020-03-23 17:14:21 -0400

    ready for deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDB();
 
 app.use(express.json({ extended: false })) //to access req.body
 
-const PORT = process.env.PORT || 5000;
+
 
 
 //Define Routes
@@ -24,5 +24,7 @@ app.use(express.static('client/build'))
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, "/client/build/index.html"))
 })
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
